ca3d2bbd66930c488e578eaefa0a3f33e63b0205
VeryCoolPatrick <culmerpatrick@gmail.com>
2023-08-17 13:35:25 +0100

    Update to unicode v15.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ module_path = os.path.join(os.path.dirname(__file__), "emoji_data_python.py")
 
 setuptools.setup(
     name="emoji_data_python",
-    version="DEV",
+    version="1.5.1",
     url="https://github.com/alexmick/emoji-data-python/",
 
     author="Alexander Micklewright",
